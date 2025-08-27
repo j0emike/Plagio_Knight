fc4d835f1a28acf36ca3e975c64714743635d7d8
j0emike <josepe0607@gmail.com>
2025-08-27 08:53:10 -0600

    Comentarios varios

---
+++ Assets/Code/MovementPlayer.cs
@@ -0,0 +1,22 @@
+//Librerias - Funciones prestadas de otros scripts
+using UnityEngine;
+
+//Public - Da permiso de usar su informacion
+     //class - forma de declararla
+           //MovementPlayer - Nombre del Script
+                          //: - Herencia entre la clase y la clase que le hereda  
+public class MovementPlayer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Start");
+    }
+
+    // Update is called once per frame
+    // Funciona mejor o peor dependiente de la PC
+    void Update()
+    {
+        print("Updating");
+    }
+}
