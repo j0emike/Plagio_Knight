8b0775aab768515a1355e6bae88fe2c7795671b8
j0emike <josepe0607@gmail.com>
2025-09-03 08:59:31 -0600

    🚧

--- Assets/Code/MovementPlayer.cs
+++ Assets/Code/MovementPlayer.cs
@@ -1,3 +1,9 @@
+///
+/// Made by j0emike
+/// Script para movimiento del jugador
+/// 
+///
+
 //Librerias - Funciones prestadas de otros scripts
 using UnityEngine;
 
@@ -7,16 +13,34 @@ using UnityEngine;
                           //: - Herencia entre la clase y la clase que le hereda  
 public class MovementPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Variables
+    public Transform transformPlayer;
+    public Rigidbody2D rigidBody2DPlayer;
+
     void Start()
     {
         print("Start");
     }
 
-    // Update is called once per frame
+
     // Funciona mejor o peor dependiente de la PC
     void Update()
     {
-        print("Updating");
+        //transform ignora las fisicas
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("Voy a la izquierda");
+            transformPlayer.position = Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("Voy a la derecha");
+            transformPlayer.position = Vector3.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("Voy hacia arriba");
+            transformPlayer.position = Vector3.up;
+        }
     }
 }
