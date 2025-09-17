dfa02ac6399be838fb33237df9fe82da163b9e80
j0emike <josepe0607@gmail.com>
2025-09-17 00:44:12 -0600

    🚧

--- Assets/Code/MovementPlayer.cs
+++ Assets/Code/MovementPlayer.cs
@@ -13,11 +13,11 @@ using UnityEngine;
                           //: - Herencia entre la clase y la clase que le hereda  
 public class MovementPlayer : MonoBehaviour
 {
-    //Variables
     public Transform transformPlayer;
     public Rigidbody2D rigidBody2DPlayer;
     public SpriteRenderer spriteRenderer;
     public bool saltoValido = true;
+    public float fuerzaSalto = 7f;
 
     void Start()
     {
@@ -25,39 +25,34 @@ public class MovementPlayer : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-
-    // Funciona mejor o peor dependiente de la PC
     void Update()
     {
-        //transform ignora las fisicas
-        
         if (Input.GetKey(KeyCode.A))
         {
             print("Voy a la izquierda");
             rigidBody2DPlayer.AddForce(Vector2.left * 5f);
-            //transformPlayer.position = Vector3.left;
         }
         else if (Input.GetKey(KeyCode.D))
         {
             print("Voy a la derecha");
             rigidBody2DPlayer.AddForce(Vector2.right * 5f);
-            //transformPlayer.position = Vector3.right;
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W)) 
         {
-            print("Voy hacia arriba");
+            print("Estoy saltando");
             if (saltoValido)
             {
-                rigidBody2DPlayer.AddForce(Vector2.up * 0.5f, ForceMode2D.Impulse);
+                rigidBody2DPlayer.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+                saltoValido = false; 
             }
-            //transformPlayer.position = Vector3.up;
         }
-        
     }
 
-    //Detecta una Colision
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            saltoValido = true;
+        }
     }
 }
